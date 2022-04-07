664baede0b5e3f9e438b680e097df5847d95e3f6
Yafei Wang <64439033+ricecakeM@users.noreply.github.com>
2022-04-07 11:10:19 -0700

    Update RedisConnection.java

--- Consumer/src/main/java/RedisConnection.java
+++ Consumer/src/main/java/RedisConnection.java
@@ -19,15 +19,4 @@ public class RedisConnection {
         poolConfig.setBlockWhenExhausted(true);
         return poolConfig;
     }
-//    private Jedis jedis;
-//
-//    public RedisConnection() {
-//            this.jedis = new Jedis("hostname", 6379);
-//            this.jedis.auth("password");
-//            System.out.println("Connected to Redis");
-//    }
-//
-//    public Jedis getJedis() {
-//        return this.jedis;
-//    }
 }
